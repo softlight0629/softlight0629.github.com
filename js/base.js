59c3cfdeb89b903d809fe0adbaa0e11256f18f27
softlight0629 <softlight0629@gmail.com>
2013-01-05 16:10:55 +0800

    add extend Class method

--- js/base.js
+++ js/base.js
@@ -78,6 +78,85 @@
             tempClass.prototype = o;
 
             return (new tempClass());
+        };
+
+        extend = function(beExtendObj, extendObj1, extendObj2) {
+            var a = arguments,
+                i,
+                p,
+                beExtendObj,
+                extendObj;
+
+                if (a.length == 1) {
+                    beExtendObj = this;
+                    i = 0;
+                } else {
+                    beExtendObj = a[0];
+                    i = 1;
+                }
+
+                for (; i < a.length; i++) {
+                    extendObj = a[i];
+                    for (p in extendObj) {
+                        var src = beExtendObj[p];
+                        var obj = extendObj[p];
+
+                        if (src === obj) {
+                            continue;
+                        }
+
+                        if (obj && isObject(obj) && !isArray(obj) && !obj.nodeType && !isFunction(obj)) {
+                            src = beExtendObj[p] || {};
+                            beExtendObj[p] = extend(src, obj || (obj.length != null) || [] : {});
+                        } else if (obj != undefined) {
+                            beExtendObj[p] = obj;
+                        }
+                    }
+                }
+
+                return beExtendObj;
+        };
+
+        Class = function() {
+            var length = arguments.length;
+            var option = arguments[length - 1];
+
+            option.init = options.init || function() {};
+
+            if (length === 2) {
+
+                var superClass = arguments[0].extend;
+
+                var tempClass = function() {}
+                tempClass.prototype = superClass;
+
+                var subClass = function() {
+                    this.init.apply(this, arguments);
+                }
+
+                subClass.superClass = superClass;
+
+
+                subClass.prototype = new tempClass();
+                subClass.prototype.constructor = subClass;
+
+                extend(subClass.prototype, option);
+
+                subClass.prototype.init = function() {
+                    option.init.apply(this, arguments);
+                }
+
+                return subClass;
+
+            } else if (length === 1) {
+
+                var newClass = function() {
+                    return this.init.apply(this, arguments);
+                }
+
+                newClass.prototype = option;
+                return newClass;
+            }
         }
 
 })();
